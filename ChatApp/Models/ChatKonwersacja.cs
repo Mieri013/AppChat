846107339c696d253be38cf5afc67e31fca8e2a5
Alicja Kapała <waifuofmarin@gmail.com>
2023-03-15 10:11:30 +0100

    Chat Konwersacji - Kontrolka Użytkownika i Model

---
+++ ChatApp/Models/ChatKonwersacja.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatApp.Models
+{
+    public class ChatKonwersacja
+    {
+        public string ImieKontakt { get; set; }
+        public string OtrzymanaWiadomosc { get; set; }
+        public string MsgReceivedOn { get; set; }
+        public string WyslanaWiadomosc { get; set; }
+        public string MsgSentOn { get; set; }
+        public bool JestOtrzymana { get; set; }
+        public bool WiadZawieraOdp { get; set; }
+    }
+}
